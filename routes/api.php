cc7a898dcb32b6df437b749bb3c47f178e3c9472
mark <markpackham1@gmail.com>
2021-07-12 15:42:21 +0100

    add login functionality

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 // Route::resource('products',ProductController::class);
 
 Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/products', [ProductController::class, 'index']);
 
